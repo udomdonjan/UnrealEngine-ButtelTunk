84d9164a6041f55b6ecddf7c85f6d14fc1e30082
Matt <mattmillergraphics@gmail.com>
2017-01-11 20:13:16 +0000

    Initial - Unreal Gen'd Files

---
+++ ButtelTunk/Source/ButtelTunk/ButtelTunkGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ButtelTunkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUTTELTUNK_API AButtelTunkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
