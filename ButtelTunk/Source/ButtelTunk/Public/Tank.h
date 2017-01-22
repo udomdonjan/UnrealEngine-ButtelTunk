bdeb94e530c575b42bdb842669aef4b44dcc1943
Matt <mattmillergraphics@gmail.com>
2017-01-22 13:57:01 +0000

    Delegate aim to own component

--- ButtelTunk/Source/ButtelTunk/Public/Tank.h
+++ ButtelTunk/Source/ButtelTunk/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -13,6 +14,10 @@ class BUTTELTUNK_API ATank : public APawn
 public:
 
 	void AimAt(FVector HitLocation);
+
+protected: 
+
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
