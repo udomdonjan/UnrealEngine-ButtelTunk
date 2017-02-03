682283ab894607744fe4bc18f56af36167c8c371
Matt <mattmillergraphics@gmail.com>
2017-02-03 12:53:40 +0000

    AI can fire again!

--- ButtelTunk/Source/ButtelTunk/Private/TankAIController.cpp
+++ ButtelTunk/Source/ButtelTunk/Private/TankAIController.cpp
@@ -25,7 +25,7 @@ void ATankAIController::Tick(float DeltaSeconds)
 		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
 		//Fire if ready
-		//ControlledTank->Fire(); // limit fire rate.
+		ControlledTank->Fire(); // limit fire rate.
 
 	}
 
