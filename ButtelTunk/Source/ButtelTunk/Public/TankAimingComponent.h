a432e7d373c1694b94f2d045c6155c520a18c346
Matt <mattmillergraphics@gmail.com>
2017-02-05 19:45:33 +0000

    Crosshair dynamic colouring

--- ButtelTunk/Source/ButtelTunk/Public/TankAimingComponent.h
+++ ButtelTunk/Source/ButtelTunk/Public/TankAimingComponent.h
@@ -5,6 +5,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+//Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 // Forward declaration 
 class UTankBarrel; 
 class UTankTurret;
@@ -23,6 +32,11 @@ public:
 	void SetTurretReferance(UTankTurret* TurretToSet);
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Aiming;
 
 private:
 
